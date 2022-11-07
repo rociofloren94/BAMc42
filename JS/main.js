f73bbbd9311e218433fd28270b3132cb7bd78bbe
rociofloren94 <rociocruz894@gmail.com>
2022-11-06 21:43:06 -0300

    cambios

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+/*prompt y alert bienvenido (quedo funcionando)
+
+let entrada = prompt("Ingresa tu nombre");
+let salida = entrada + " " + "Bienvenido/a a mi sitio web!";
+alert(salida);
+*/
+
+//prompt edad (quedo funcionando)
+
+let b = prompt("Ingresa tu edad")
+if (b < 18) {
+  alert("Eres menor de edad");
+} else {
+  alert("Eres mayor de edad");
+}
